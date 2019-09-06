e51e4bb07284579a37ddd17dfc32ed4d4d2fd25e
pandaCure <zzyamor@163.com>
2019-09-06 17:34:37 +0800

    [0][feat][zhongyou]  add JSXText VariableDeclarator Property rules

--- packages/eslint-plugin-check-diff-chinese/__tests__/eslint-plugin-check-diff-chinese.test.js
+++ packages/eslint-plugin-check-diff-chinese/__tests__/eslint-plugin-check-diff-chinese.test.js
@@ -14,7 +14,13 @@ const parserOptions = {
 }
 const ruleTester = new RuleTester({ parserOptions })
 ruleTester.run('check-diff-chinese', rule, {
-  valid: [{ code: "<App foo={'影片'} />;" }],
+  valid: [
+    { code: "<App foo={'影片'} />;" },
+    { code: '<App>影片</App>;' },
+    { code: "const a = '影片'" },
+    { code: "const a = () => { const s = '影片' }" },
+    { code: "const a = { b: '影片', c: { d: '影片' } }" }
+  ],
   invalid: [
     {
       code: "<App foo={'视频'} />;",
@@ -28,6 +34,38 @@ ruleTester.run('check-diff-chinese', rule, {
       output: "<App foo={'影片'} />;",
       errors: [
         { message: 'foo --->  視頻 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' }
+      ]
+    },
+    {
+      code: '<App>視頻</App>;',
+      output: '<App>影片</App>;',
+      errors: [{ message: '視頻 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' }]
+    },
+    {
+      code: "const a = '視頻'",
+      output: "const a = '影片'",
+      errors: [{ message: '視頻 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' }]
+    },
+    {
+      code: "const b = () => { const a = '視頻' }",
+      output: "const b = () => { const a = '影片' }",
+      errors: [{ message: '視頻 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' }]
+    },
+    {
+      code: "const b = { a: '視頻' }",
+      output: "const b = { a: '影片' }",
+      errors: [{ message: '視頻 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' }]
+    },
+    {
+      code:
+        "const b = { a: { a: { a: { a: '視頻' }, b: { a: '視頻' } }, b: { a: '視頻' } }, b: { a: '視頻' } }",
+      output:
+        "const b = { a: { a: { a: { a: '影片' }, b: { a: '影片' } }, b: { a: '影片' } }, b: { a: '影片' } }",
+      errors: [
+        { message: '視頻 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' },
+        { message: '視頻 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' },
+        { message: '視頻 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' },
+        { message: '視頻 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' }
       ]
     }
   ]
--- packages/eslint-plugin-check-diff-chinese/lib/eslint-plugin-check-diff-chinese.js
+++ packages/eslint-plugin-check-diff-chinese/lib/eslint-plugin-check-diff-chinese.js
@@ -1,5 +1,5 @@
 'use strict'
-const errText = ['視頻', '视频']
+const errorText = require('../config/config')
 const RuleOfCheckDiffChinese = {
   meta: {
     type: 'suggestion',
@@ -8,21 +8,80 @@ const RuleOfCheckDiffChinese = {
     schema: {}
   },
   create(context) {
-    console.log(`------------->`)
     return {
       JSXAttribute(node) {
         const propName = node.name && node.name.name
         const value = node.value
         if (value && value.expression && value.expression.value) {
-          if (errText.includes(value.expression.value)) {
+          const JSXAttributeText = value.expression.value
+          if (errorText.has(JSXAttributeText)) {
             context.report({
               node,
               message: `${propName} --->  {{ str }} 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～`,
               data: {
-                str: value.expression.value
+                str: JSXAttributeText
               },
               fix(fixer) {
-                return fixer.replaceTextRange(value.expression.range, "'影片'")
+                return fixer.replaceTextRange(
+                  value.expression.range,
+                  `'${errorText.get(JSXAttributeText)}'`
+                )
+              }
+            })
+          }
+        }
+      },
+      JSXText(node) {
+        const value = node.value
+        if (errorText.has(value)) {
+          context.report({
+            node,
+            message: `{{ str }} 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～`,
+            data: {
+              str: value
+            },
+            fix(fixer) {
+              return fixer.replaceTextRange(
+                node.range,
+                `${errorText.get(value)}`
+              )
+            }
+          })
+        }
+      },
+      VariableDeclarator(node) {
+        const value = node.init && node.init.value
+        if (errorText.has(value)) {
+          context.report({
+            node,
+            message: `{{ str }} 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～`,
+            data: {
+              str: value
+            },
+            fix(fixer) {
+              return fixer.replaceTextRange(
+                node.init.range,
+                `'${errorText.get(value)}'`
+              )
+            }
+          })
+        }
+      },
+      Property(node) {
+        if (node.value && node.value.type === 'Literal') {
+          const value = node.value.value
+          if (errorText.has(value)) {
+            context.report({
+              node,
+              message: `{{ str }} 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～`,
+              data: {
+                str: value
+              },
+              fix(fixer) {
+                return fixer.replaceTextRange(
+                  node.value.range,
+                  `'${errorText.get(value)}'`
+                )
               }
             })
           }
