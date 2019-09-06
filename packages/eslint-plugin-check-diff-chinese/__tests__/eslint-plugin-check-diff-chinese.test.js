27f3ba5c1fdb4f09a5e5b87f332558421b984d45
pandaCure <zzyamor@163.com>
2019-09-06 14:57:42 +0800

    [0][feat][zhongyou] add JSX attr check chinese

--- packages/eslint-plugin-check-diff-chinese/__tests__/eslint-plugin-check-diff-chinese.test.js
+++ packages/eslint-plugin-check-diff-chinese/__tests__/eslint-plugin-check-diff-chinese.test.js
@@ -1,7 +1,34 @@
-'use strict';
+'use strict'
 
-const eslintPluginCheckDiffChinese = require('..');
+const RuleTester = require('eslint').RuleTester
+const rule = require('../lib/eslint-plugin-check-diff-chinese')[
+  'check-diff-chinese'
+]
 
-describe('eslint-plugin-check-diff-chinese', () => {
-    it('needs tests');
-});
+const parserOptions = {
+  ecmaVersion: 2018,
+  sourceType: 'module',
+  ecmaFeatures: {
+    jsx: true
+  }
+}
+const ruleTester = new RuleTester({ parserOptions })
+ruleTester.run('check-diff-chinese', rule, {
+  valid: [{ code: "<App foo={'影片'} />;" }],
+  invalid: [
+    {
+      code: "<App foo={'视频'} />;",
+      output: "<App foo={'影片'} />;",
+      errors: [
+        { message: 'foo --->  视频 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' }
+      ]
+    },
+    {
+      code: "<App foo={'視頻'} />;",
+      output: "<App foo={'影片'} />;",
+      errors: [
+        { message: 'foo --->  視頻 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～' }
+      ]
+    }
+  ]
+})
--- packages/eslint-plugin-check-diff-chinese/lib/eslint-plugin-check-diff-chinese.js
+++ packages/eslint-plugin-check-diff-chinese/lib/eslint-plugin-check-diff-chinese.js
@@ -1,21 +1,31 @@
 'use strict'
-const errText = ['标', '視頻', '视频']
+const errText = ['視頻', '视频']
 const RuleOfCheckDiffChinese = {
   meta: {
-    type: 'suggestion'
+    type: 'suggestion',
+    fixable: 'code',
+    docs: {},
+    schema: {}
   },
   create(context) {
+    console.log(`------------->`)
     return {
-      Literal: node => {
-        const { value } = node
-        if (errText.includes(value)) {
-          context.report({
-            node,
-            message: '{{ str }} 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～',
-            data: {
-              str: value
-            }
-          })
+      JSXAttribute(node) {
+        const propName = node.name && node.name.name
+        const value = node.value
+        if (value && value.expression && value.expression.value) {
+          if (errText.includes(value.expression.value)) {
+            context.report({
+              node,
+              message: `${propName} --->  {{ str }} 是包含错用错别字 请修改 （づ￣3￣）づ╭❤～`,
+              data: {
+                str: value.expression.value
+              },
+              fix(fixer) {
+                return fixer.replaceTextRange(value.expression.range, "'影片'")
+              }
+            })
+          }
         }
       }
     }
